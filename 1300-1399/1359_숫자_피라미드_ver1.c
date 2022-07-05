775d1eb25c5ed06fed3e8e21ccc138135c2086b7
sfK <noonsung@daum.net>
2022-07-05 20:38:08 +0900

    c_programing

---
+++ 1300-1399/1359_숫자_피라미드_ver1.c
@@ -0,0 +1,27 @@
+// 길이 n이 입력되면 다음과 같은 숫자 피라미드를 출력한다.
+// 예)
+// n이 5이면
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j, n;
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
